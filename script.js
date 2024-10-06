674b7ba7daa3dbabd6a8cc70d621501b17c8375b
Iflaq khurshid <112414810+Iflaqbhat@users.noreply.github.com>
2024-10-06 12:36:07 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,10 +1,16 @@
 const byteSize = (str) => {
-	let x =str.length;
+	if(str==''){
+		return 0;
+	}else{
+		let x =str.length;
     return x;
+	}
+	
+	
   // write your code herere
 	
 };
 
 // Do not change the code below
 const str = prompt("Enter some string.");
-alert(byteSize(str));
+//alert(byteSize(str));
